5bb7118b166c76f9310dc0b52c7ab571bb1731cb
Patrick <patrickhong21@hotmail.com>
2022-02-12 11:14:28 -0800

    Initial Commit

---
+++ duck_news_getter.py
@@ -0,0 +1,5 @@
+def get_news() -> str:
+    '''
+    Produces a few sentences of news with url appended at the end.
+    '''
+    pass
---
+++ duck_pic_getter.py
@@ -0,0 +1,4 @@
+def get_link() -> str:
+    '''
+    Gives link of random duck pic
+    '''
