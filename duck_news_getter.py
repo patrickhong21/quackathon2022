ec32f45f7ab829e1a31f3b97305f83e89634072d
Patrick <patrickhong21@hotmail.com>
2022-02-12 12:33:55 -0800

    Done pic getter and formatted news getter

--- duck_news_getter.py
+++ duck_news_getter.py
@@ -2,38 +2,33 @@ from serpapi import GoogleSearch
 from random import randint
 from typing import List
 
+
 def get_news() -> List[str]:
     '''
     Produces a few sentences of news with url appended at the end.
-    ''' 
-    print(scrape_news())
-    return scrape_news()
+    '''
+    news_results = scrape_news()
+
+    rand_int = randint(0, len(news_results) - 1)
+    news_snippet = news_results[rand_int]["snippet"]
+    news_url = news_results[rand_int]["link"]
+
+    return [news_snippet, news_url]
 
 
-def scrape_news() -> List[str]:
+def scrape_news() -> dict:
     '''
-    api get json
+    Get JSON in dict format from API
     '''
+
     params = {
     "q": "duck animal",
     "tbm": "nws",
     "api_key": "7babd63756f443d283ecd3fd67e39fe3af581203b500dec3e42b47ba6b1e2ca8"
     }
 
-
     search = GoogleSearch(params)
     results = search.get_dict()
     news_results = results['news_results']
 
-    rand_Integer = randint(0,len(news_results)-1)
-
-    news_snippet = news_results[rand_Integer]["snippet"]
-    news_url = news_results[rand_Integer]["link"]
-
-
-
-    return [news_snippet,news_url]
-    
-get_news()
-
-
+    return news_results
--- duck_pic_getter.py
+++ duck_pic_getter.py
@@ -1,4 +1,34 @@
+from serpapi import GoogleSearch
+from random import randint
+
+
 def get_link() -> str:
     '''
     Gives link of random duck picture
     '''
+    images_results = scrape_image()
+
+    rand_int = randint(0, len(images_results) - 1)
+    image_link = images_results[rand_int]['original']
+
+    print(image_link)
+
+    return image_link
+
+
+def scrape_image() -> str:
+    '''
+    Get JSON in dict format from API
+    '''
+    params = {
+        "q": "Duck Animal",
+        "tbm": "isch",
+        "ijn": "0",
+        "api_key": "e191c33349d3ba314d5edd332fc577df540a383576e967f15a45f83a3d2be231"
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    images_results = results['images_results']
+
+    return images_results
